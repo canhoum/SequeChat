f182585d0e7534db4c4034dc118c9d7b8a789cb4
L3gend776 <aluno23878@ipt.pt>
2025-01-30 04:35:08 +0000

    Login e Logout

--- app/src/main/java/pt/ipt/dam/sequechat/MainActivity.kt
+++ app/src/main/java/pt/ipt/dam/sequechat/MainActivity.kt
@@ -1,5 +1,7 @@
 package pt.ipt.dam.sequechat
 import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
 import java.net.HttpURLConnection
 import java.net.URL
 import kotlinx.coroutines.*
@@ -7,50 +9,64 @@ import org.json.JSONObject
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import pt.ipt.dam.sequechat.activities.PreSignIn
+import pt.ipt.dam.sequechat.activities.SignInActivity
 import java.io.OutputStreamWriter
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("SuspiciousIndentation")
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        // Receber os dados do Intent
-        val name = intent.getStringExtra("name")
-        val email = intent.getStringExtra("email")
-        val password = intent.getStringExtra("password")
 
-        var email_login = intent.getStringExtra("email_login")
-        var password_login = intent.getStringExtra("password_login")
+            super.onCreate(savedInstanceState)
+            enableEdgeToEdge()
+            setContentView(R.layout.activity_main)
+            // Receber os dados do Intent
+            val name = intent.getStringExtra("name")
+            val email = intent.getStringExtra("email")
+            val password = intent.getStringExtra("password")
 
-        if(email_login != null && password_login != null){
-            checkLoginToSheety(email_login,password_login)
-        }else{
-            println("Erro ao receber os dados de login")
-        }
 
-        // Verificar se os dados não são nulos e chamar a função
-        if (name != null && email != null && password != null) {
-            postToSheety(name, email, password)
-        } else {
-            println("Erro ao receber os dados.")
-        }
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-        val button: Button = findViewById(R.id.btnteste)
-            button.setOnClickListener{
-                fetchSheetyData()
+            // Verificar se os dados não são nulos e chamar a função
+            if (name != null && email != null && password != null) {
+                postToSheety(name, email, password)
+            } else {
+                println("Erro ao receber os dados.")
             }
 
+            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+                insets
+            }
+            val button: Button = findViewById(R.id.btnteste)
+            val button_logout: Button = findViewById(R.id.buttonLogOut)
+            button.setOnClickListener {
+                fetchSheetyData()
+            }
+            button_logout.setOnClickListener{
+                logout(this)
+            }
     }
+
+    fun logout(context: Context) {
+        // Usar SharedPreferences para limpar o estado de login
+        val sharedPreferences = context.getSharedPreferences("AppPreferences", Context.MODE_PRIVATE)
+        sharedPreferences.edit().putBoolean("IsLoggedIn", false).apply()
+        sharedPreferences.edit().remove("UserId").apply()
+
+        // Redirecionar para a tela de login
+        showToast("Sessão terminada!")
+        val loginIntent = Intent(context, PreSignIn::class.java)
+        loginIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+        context.startActivity(loginIntent)
+    }
+
+
     fun fetchSheetyData() {
         val url = "https://api.sheety.co/182b17ec2dcc0a8d3be919b2baff9dfc/sequechat/folha1"
 
@@ -84,6 +100,9 @@ class MainActivity : AppCompatActivity() {
                 e.printStackTrace()
             }
         }
+    }
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
     fun postToSheety(name: String, email: String, password: String) {
         val url = "https://api.sheety.co/182b17ec2dcc0a8d3be919b2baff9dfc/sequechat/folha1"
@@ -138,63 +157,5 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    //Função para realizar o login
-    fun checkLoginToSheety(email: String, password: String) {
-        val url = "https://api.sheety.co/182b17ec2dcc0a8d3be919b2baff9dfc/sequechat/folha1"
-
-        CoroutineScope(Dispatchers.IO).launch {
-            try {
-                val urlObj = URL(url)
-                val connection = urlObj.openConnection() as HttpURLConnection
-
-                // Configuração da requisição GET para obter os dados
-                connection.requestMethod = "GET"
-                connection.setRequestProperty("Content-Type", "application/json")
-
-                val responseCode = connection.responseCode
-                if (responseCode == HttpURLConnection.HTTP_OK) {
-                    val response = connection.inputStream.bufferedReader().use { it.readText() }
-                    val jsonResponse = JSONObject(response)
-
-                    // Verifica se o email e password coincidem com algum utilizador registado
-                    val usersArray = jsonResponse.getJSONArray("folha1")
-                    var loginSuccessful = false
-
-                    for (i in 0 until usersArray.length()) {
-                        val user = usersArray.getJSONObject(i)
-                        val registeredEmail = user.getString("email")
-                        val registeredPassword = user.getString("password")
-
-                        if (email == registeredEmail && password == registeredPassword) {
-                            loginSuccessful = true
-                            break
-                        }
-                    }
-
-                    withContext(Dispatchers.Main) {
-                        if (loginSuccessful) {
-                            println("Login bem-sucedido!")
-                            // Redireciona para a nova atividade (substituir pela tua view final)
-                            //startActivity(Intent(this@MainActivity, SomeOtherActivity::class.java))
-                            //finish() // Fecha a MainActivity
-                        } else {
-                            println("Email ou password incorretos.")
-                        }
-                    }
-                } else {
-                    withContext(Dispatchers.Main) {
-                        println("Erro ao conectar ao servidor: Código $responseCode")
-                    }
-                }
-
-                connection.disconnect()
-            } catch (e: Exception) {
-                withContext(Dispatchers.Main) {
-                    println("Erro: ${e.message}")
-                }
-                e.printStackTrace()
-            }
-        }
-    }
 
 }
--- app/src/main/java/pt/ipt/dam/sequechat/activities/PreSignIn.kt
+++ app/src/main/java/pt/ipt/dam/sequechat/activities/PreSignIn.kt
@@ -1,12 +1,20 @@
 package pt.ipt.dam.sequechat.activities
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.json.JSONObject
 import pt.ipt.dam.sequechat.MainActivity
 import pt.ipt.dam.sequechat.databinding.ActivitySignInBinding
+import java.net.HttpURLConnection
+import java.net.URL
 
 class PreSignIn : AppCompatActivity() {
 
@@ -25,19 +33,13 @@ class PreSignIn : AppCompatActivity() {
     private fun setListeners(){
         // Configurar o evento do botão "Entrar"
         binding.buttonEntrar.setOnClickListener {
-            Log.d("SignInActivity","AAAAAAAAAAAAAAAAA")
             val email = binding.inputEmail.text.toString().trim()
             val password = binding.inputPassword.text.toString().trim()
 
             if (email.isEmpty() || password.isEmpty()) {
                 showToast("Preencha todos os campos!")
             } else {
-                // Enviar os dados de login para a MainActivity
-                val intent = Intent(this, MainActivity::class.java).apply {
-                    putExtra("email_login", email)
-                    putExtra("password_login", password)
-                }
-                startActivity(intent)
+                checkLoginToSheety(email,password)
             }
         }
 
@@ -50,5 +52,67 @@ class PreSignIn : AppCompatActivity() {
 
     private fun showToast(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    //Função para realizar o login
+    fun checkLoginToSheety(email: String, password: String) {
+        val url = "https://api.sheety.co/182b17ec2dcc0a8d3be919b2baff9dfc/sequechat/folha1"
+
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val urlObj = URL(url)
+                val connection = urlObj.openConnection() as HttpURLConnection
+
+                // Configuração da requisição GET para obter os dados
+                connection.requestMethod = "GET"
+                connection.setRequestProperty("Content-Type", "application/json")
+
+                val responseCode = connection.responseCode
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    val response = connection.inputStream.bufferedReader().use { it.readText() }
+                    val jsonResponse = JSONObject(response)
+
+                    // Verifica se o email e password coincidem com algum utilizador registado
+                    val usersArray = jsonResponse.getJSONArray("folha1")
+                    var loginSuccessful = false
+
+                    for (i in 0 until usersArray.length()) {
+                        val user = usersArray.getJSONObject(i)
+                        val registeredEmail = user.getString("email")
+                        val registeredPassword = user.getString("password")
+
+                        if (email == registeredEmail && password == registeredPassword) {
+                            loginSuccessful = true
+                            break
+                        }
+                    }
+
+                    withContext(Dispatchers.Main) {
+                        if (loginSuccessful) {
+                            showToast("Login Bem sucedido")
+                            val sharedPreferences = this@PreSignIn.getSharedPreferences("AppPreferences", Context.MODE_PRIVATE)
+                            sharedPreferences.edit().putBoolean("IsLoggedIn", true).apply()
+                            // Redireciona para a nova atividade (substituir pela tua view final)
+                            startActivity(Intent(this@PreSignIn, MainActivity::class.java))
+                            finish() //
+                        } else {
+                            showToast("Credenciais erradas!")
+
+                        }
+                    }
+                } else {
+                    withContext(Dispatchers.Main) {
+                        println("Erro ao conectar ao servidor: Código $responseCode")
+                    }
+                }
+
+                connection.disconnect()
+            } catch (e: Exception) {
+                withContext(Dispatchers.Main) {
+                    println("Erro: ${e.message}")
+                }
+                e.printStackTrace()
+            }
+        }
     }
 }
